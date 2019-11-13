1e02d5a7bfec710523581d040af078a730f27eb0
Fabulous_Rachael <rachael.whitefield@gmail.com>
2019-11-13 14:51:19 -0700

    created a hook for day usage

--- react-weather/src/App.js
+++ react-weather/src/App.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container, Row, Col } from "reactstrap"; 
 import SearchBar from "./components/SearchBar";
 import DayCard from "./components/DayCard";
 import DayDetails from "./components/DayDetails";
 import sampleData from "./data/sample.json";
 
-function App() {
+const App = () => {
+  const [day, setDay] = useState("Monday");
+  const [data, setData] = useState({
+    days: [],
+    location: "Denver, CO",
+    selectedDay: null,
+    searchTerm: ""
+  });
+
   return (
     <Container>
       <Row>
-        <Col md={8}><h1>Weather for Somewhere</h1></Col>
+        <Col md={8}><h1>Weather for {day}</h1></Col>
         <Col md={4}><SearchBar /></Col>
       </Row>
       <Row>
